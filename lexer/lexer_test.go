5631dba7ab240a8578702a409946ed4da39aa49e
Emily Yao <emily2004.yao@gmail.com>
2024-05-16 14:21:03 -0400

    (1.3) the lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,190 @@
+package lexer
+
+import (
+	"testing"
+	"github.com/monkey/token"
+)
+
+type Lexer struct {
+	input		 string
+	position	 int
+	readPosition int
+	ch			 byte
+}
+
+// Creates a new Lexer
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// Tests tokens parsed from input against expected tokens
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+`
+
+	tests := []struct {
+		expectedType	token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got =%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+// Parse text to get next token
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	// Consume whitespace since Monkey doesn't care about whitespace
+	l.skipWhitespace()
+
+	// Consume a character and assign it to a token type
+	switch l.ch {
+		case '=':
+			tok = newToken(token.ASSIGN, l.ch)
+		case ';':
+			tok = newToken(token.SEMICOLON, l.ch)
+		case '(':
+			tok = newToken(token.LPAREN, l.ch)
+		case ')':
+			tok = newToken(token.RPAREN, l.ch)
+		case ',':
+			tok = newToken(token.COMMA, l.ch)
+		case '+':
+			tok = newToken(token.PLUS, l.ch)
+		case '{':
+			tok = newToken(token.LBRACE, l.ch)
+		case '}':
+			tok = newToken(token.RBRACE, l.ch)
+		case 0:
+			tok.Literal = ""
+			tok.Type = token.EOF
+		default:
+			// Consume tokens which are longer than 1 character
+			if isLetter(l.ch) {
+				tok.Literal = l.readIdentifier()
+				tok.Type = token.LookupIdent(tok.Literal)
+				return tok
+			} else if isDigit(l.ch) {
+				tok.Type = token.INT
+				tok.Literal = l.readNumber()
+				return tok
+			} else {
+				tok = newToken(token.ILLEGAL, l.ch)
+			}
+	}
+
+	l.readChar()
+	return tok
+}
+
+// Creates new token given the type and value of the token
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// Reads a single character
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) { // end of line or no character
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1 // consume character
+}
+
+// Reads strings of identifiers
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position] // slice of input containing identifier
+}
+
+// Reads blocks of digits
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position] // slice of input containing number
+}
+
+// Skips over blocks of whitespace
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// Checks if the passed in byte is uppercase or lowercase letter, or an underscore
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// Checks if the passed in byte is a digit between 0 and 9.
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
