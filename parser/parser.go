f6a55356dc2178fbf58d6d3ddf1c36d613f7d62e
Emily Yao <emily2004.yao@gmail.com>
2024-05-18 18:05:42 -0400

    (2.5) parsing return statements)

--- parser/parser.go
+++ parser/parser.go
@@ -51,6 +51,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 		case token.LET:
 			return p.parseLetStatement()
+		case token.RETURN:
+			return p.parseReturnStatement()
 		default:
 			return nil
 	}
@@ -76,6 +78,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	return stmt
+}
+
+// Parses expression in the form of 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: skip all expressions until we get to semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt 
 }
 
 // Get next token
